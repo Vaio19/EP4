4f371e354d2fe84aa9aec1a0797d01627a11dec0
Vaio19 <vaio.sony19@hotmail.com>
2023-02-12 13:00:29 +0700

    router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk=require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,16 @@ app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine","ejs");
+
+productRouter.route("/").get((req,res) => {
+    res.send("Hello World !! I'm Product");
+});
+
+productRouter.route("/1").get((req,res) => {
+    res.send("Hello World !! I'm Product 1");
+});
+
+app.use("/products", productRouter)
 
 app.get("/", (req,res) =>{
     
